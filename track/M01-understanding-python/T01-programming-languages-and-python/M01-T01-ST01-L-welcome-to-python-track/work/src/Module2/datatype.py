823b2d692f1e8506ac4ba99603173c0a78ef817f
Oduru Yashwanth Reddy <oduruyashwanthreddy@Odurus-MacBook-Air.local>
2026-08-03 17:23:45 +0530

    Day2Changes

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/Module2/datatype.py
@@ -0,0 +1,10 @@
+# Creating variables
+name="yashu"
+course_duration=30
+course_rating=4.8
+is_enrolled=True
+# checking data types
+print(type(name))
+print(type(course_duration))
+print(type(course_rating))
+print(type(is_enrolled))
