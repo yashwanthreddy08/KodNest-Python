00ec0e7099b494cdd5bd49884e7a9fd645778d23
yashwanthreddy08 <yashwanthreddy0808146@gmail.com>
2026-08-06 17:26:07 +0530

    Read and Display Elements

---
+++ track/M01-understanding-python/M02/T02/src/r_d_l_e.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number:"))
+numbers = []
+# Read n integers and add them to the list
+for i in range(1,n+1):
+    num=int(input("Enter a number:"))
+    numbers.append(num)
+print(numbers)
